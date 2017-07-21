1e2f2dfd8bdc8f655bc500f3ec9f73faa914b176
Justin Bradfield <bradfield.justin7@gmail.com>
2017-07-21 13:40:59 -0400

    Update 20170705225607_create_plates.rb
    Try this

--- db/migrate/20170705225607_create_plates.rb
+++ db/migrate/20170705225607_create_plates.rb
@@ -2,9 +2,8 @@ class CreatePlates < ActiveRecord::Migration[5.1]
   def change
     create_table :plates do |t|
       t.string :number
-
       t.timestamps
-    
+    end
+    add_index :plates, unique: true
   end
-  add_index :plates, unique: true
 end
